b957f4e8e4fef4c2650990e4860eb8c1a408ab5c
Darya8812 <115808845+Darya8812@users.noreply.github.com>
2024-09-10 00:19:56 +0300

    Update models.py

--- EducationSystem/courses/models.py
+++ EducationSystem/courses/models.py
@@ -11,3 +11,38 @@ class Courses(models.Model):
     class Meta:
         verbose_name = 'Курс'
         verbose_name_plural = 'Курсы'
+class Modules(models.Model):
+    course = models.ForeignKey(Courses, on_delete=models.CASCADE, related_name='modules')
+    title = models.CharField('Название модуля', max_length=250)
+    description = models.TextField('Описание модуля')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Модуль'
+        verbose_name_plural = 'Модули'
+
+class Lessons(models.Model):
+    module = models.ForeignKey(Modules, on_delete=models.CASCADE, related_name='lessons')
+    title = models.CharField('Название урока', max_length=250)
+    content = models.TextField('Содержание урока')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+
+class Assignment(models.Model):
+    lesson = models.ForeignKey(Lessons, on_delete=models.CASCADE, related_name='assignments')
+    title = models.CharField('Название задания', max_length=250)
+    description = models.TextField('Описание задания')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Задание'
+        verbose_name_plural = 'Задания'
