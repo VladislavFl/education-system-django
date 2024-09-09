6c4e4580f065433bb7594fdc99b7f8b1923d70fd
Darya8812 <115808845+Darya8812@users.noreply.github.com>
2024-09-10 00:21:57 +0300

    Update urls.py

--- EducationSystem/courses/urls.py
+++ EducationSystem/courses/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.courses_home, name='courses_home'),
-    path('create', views.create, name='create')
+    path('create_course', views.create_course, name='create_course'),
+    path('create_module/<int:course_id>/', views.create_module, name='create_module'),
+    path('create_lesson/<int:module_id>/', views.create_lesson, name='create_lesson'),
+    path('create_assignment/<int:lesson_id>/', views.create_assignment, name='create_assignment'),
 ]
