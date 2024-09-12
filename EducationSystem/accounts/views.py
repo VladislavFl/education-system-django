f533fddb03cc054ac7fa7fc3369cc37acd56369f
Darya8812 <darailtna555@gmail.com>
2024-09-13 00:49:38 +0300

    student and teacher profile

--- EducationSystem/accounts/views.py
+++ EducationSystem/accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import Group
 from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def register(request):
     if request.method == 'POST':
@@ -15,7 +16,7 @@ def register(request):
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             login(request, user)
-            return redirect('home')
+            return redirect('profile') # Перенаправление в личный кабинет
     else:
         form = UserRegisterForm()
     return render(request, 'accounts/register.html', {'form': form})
@@ -29,7 +30,16 @@ def login_view(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('home')
+                return redirect('profile')
     else:
         form = AuthenticationForm()
     return render(request, 'accounts/login.html', {'form': form})
+
+@login_required
+def profile(request):
+    user_groups = request.user.groups.values_list('name', flat=True)
+
+    if 'Teacher' in user_groups:
+        return render(request, 'accounts/teacher_profile.html', {'user': request.user})
+    else:
+        return render(request, 'accounts/student_profile.html', {'user': request.user})
