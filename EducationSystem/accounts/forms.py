19f4d199876cc3338e3c00cd995e2a906b9451a5
VladislavFl <vlad23fleganov@gmail.com>
2024-09-13 16:46:10 +0300

    Changed layout of main page, changed layout of login and register pages, try to redirect profile

--- EducationSystem/accounts/forms.py
+++ EducationSystem/accounts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={
@@ -8,21 +9,32 @@ class UserRegisterForm(UserCreationForm):
         'placeholder': 'Email'
     }))
 
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Имя пользователя'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Пароль'
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Подтверждение пароля'
+    }))
+
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
 
-        widgets = {
-            'username': forms.TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Имя пользователя'
-            }),
-            'password1': forms.PasswordInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Пароль'
-            }),
-            'password2': forms.PasswordInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Подтверждение пароля'
-            }),
-        }
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Имя пользователя'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Пароль'
+    }))
--- EducationSystem/accounts/views.py
+++ EducationSystem/accounts/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import Group
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserLoginForm
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 def register(request):
     if request.method == 'POST':
@@ -16,30 +17,50 @@ def register(request):
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
             login(request, user)
-            return redirect('profile') # Перенаправление в личный кабинет
+            return redirect('home') # Перенаправление в личный кабинет
     else:
         form = UserRegisterForm()
     return render(request, 'accounts/register.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
+        form = UserLoginForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
             if user is not None:
-                login(request, user)
-                return redirect('profile')
+                if user.is_active:
+                    login(request, user)
+                    user_groups = user.groups.values_list('name', flat=True)
+                    messages.info(user_groups)
+                    if 'Teacher' in user_groups:
+                        return redirect('teacher_profile')
+                    elif 'Student' in user_groups:
+                        return redirect('student_profile')
+                    else:
+                        return redirect('home')
+                else:
+                    messages.error(request, 'Учетная запись неактивна.')
+            else:
+                messages.error(request, 'Неправильное имя пользователя или пароль.')
     else:
-        form = AuthenticationForm()
+        form = UserLoginForm()
     return render(request, 'accounts/login.html', {'form': form})
+
+@login_required
+def teacher_profile(request):
+    return render(request, 'accounts/teacher_profile.html', {'user': request.user})
+
+@login_required
+def student_profile(request):
+    return render(request, 'accounts/student_profile.html', {'user': request.user})
 
 @login_required
 def profile(request):
     user_groups = request.user.groups.values_list('name', flat=True)
 
-    if 'Teacher' in user_groups:
-        return render(request, 'accounts/teacher_profile.html', {'user': request.user})
-    else:
+    if 'Student' in user_groups:
         return render(request, 'accounts/student_profile.html', {'user': request.user})
+    else:
+        return render(request, 'accounts/teacher_profile.html', {'user': request.user})
